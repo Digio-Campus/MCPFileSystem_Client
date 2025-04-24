d940f027146a4a3bcfe45bcbef0a2db7b9e20c14
Antonio <antonioyjuansf@gmail.com>
2025-04-24 11:56:15 +0200

    finiquitado y añadido .env

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js"
 import { ChatOpenAI } from "@langchain/openai";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { loadMcpTools } from "@langchain/mcp-adapters";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Initialize the model
 const model = new ChatOpenAI({
   temperature: 0.7,
+  model: process.env.MODEL_NAME,
   configuration: {
-    baseURL: "http://localhost:1337/v1", // Custom url
+    baseURL: process.env.BASE_URL, // Custom url
+    apiKey: "feliz jueves",
   },
 });
 
 // Connecting to an example mcp server
 const transport = new StdioClientTransport({
   command: "npx",
-  args: ["-y", "@modelcontextprotocol/server-math"],
+  args: [
+    "-y",
+    "@modelcontextprotocol/server-filesystem",
+    "./files",
+    //"/path/to/other/allowed/dir",
+  ],
 });
 
 const client = new Client({
-  name: "math-client",
+  name: "fsClient",
   version: "1.0.0",
 });
 
@@ -27,17 +36,22 @@ try {
   await client.connect(transport);
 
   // Get tools with custom configuration
-  const tools = await loadMcpTools("math", client, {
+  const tools = await loadMcpTools("fs", client, {
     throwOnLoadError: true,
     prefixToolNameWithServerName: false,
     additionalToolNamePrefix: "",
   });
+  console.log("Tools loaded:", tools.map((tool) => tool.name).join(", "));
 
   // Create and run the agent
-  const agent = createReactAgent({ llm: model, tools });
+  const agent = await createReactAgent({ llm: model, tools });
+  console.log("Agent created:", agent.name);
+
   const agentResponse = await agent.invoke({
-    messages: [{ role: "user", content: "what's (3 + 5) x 12?" }],
+    //messages: [{ role: "user", content: "por favor cuenta los archivos que hay en la ruta y dime el top 10 canciones" }],
+    messages: [{ role: "user", content: process.env.PROMPT }],
   });
+  console.log("Agent response:");
   console.log(agentResponse);
 } catch (e) {
   console.error(e);
---
+++ files/top10songs
@@ -0,0 +1,10 @@
+1. California dreamin'
+2. I want to break free
+3. I will survive
+4. I want it that way
+5. Sweet Caroline
+6. I will always love you
+7. I don't want to miss a thing
+8. My heart
+9. I will always love you
+10. blibliblibla
